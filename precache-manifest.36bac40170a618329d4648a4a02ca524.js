90dad7cceaca46c94b10fc7d8b030c231f95b15d
angelzbg <badblo0dbg@gmail.com>
2019-10-10 18:03:26 +0300

    Updates

---
+++ precache-manifest.36bac40170a618329d4648a4a02ca524.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1fd87a42eae6d92719b5f0b77fd903",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "219ab0a406e1678f19bf",
+    "url": "/portfolio/static/css/2.7c4efcaa.chunk.css"
+  },
+  {
+    "revision": "5f28d5a17df74ccd8d28",
+    "url": "/portfolio/static/css/main.61d600cc.chunk.css"
+  },
+  {
+    "revision": "219ab0a406e1678f19bf",
+    "url": "/portfolio/static/js/2.e3cb95e7.chunk.js"
+  },
+  {
+    "revision": "5f28d5a17df74ccd8d28",
+    "url": "/portfolio/static/js/main.8cb795c8.chunk.js"
+  },
+  {
+    "revision": "9821c9659a93692e486a",
+    "url": "/portfolio/static/js/runtime-main.14a56db7.js"
+  },
+  {
+    "revision": "dc209ac1b64aa121538bec404de8c05d",
+    "url": "/portfolio/static/media/2dBig.dc209ac1.png"
+  },
+  {
+    "revision": "a7362fa5e49c01c369ce06f3b784c84d",
+    "url": "/portfolio/static/media/2dMini.a7362fa5.png"
+  },
+  {
+    "revision": "ef3fb569a803e95164a77dd31e336ca4",
+    "url": "/portfolio/static/media/avatar.ef3fb569.png"
+  },
+  {
+    "revision": "fe17dbc8f6efde95f9429ebca5d28106",
+    "url": "/portfolio/static/media/backgr_honey_big_000000.fe17dbc8.png"
+  },
+  {
+    "revision": "6f17597600cf7c0b6b385280bd3f2d17",
+    "url": "/portfolio/static/media/backgr_honey_big_big_000000.6f175976.png"
+  },
+  {
+    "revision": "b971a90759c1b8155a289cac8b4a8b86",
+    "url": "/portfolio/static/media/background_project_avatar_hover.b971a907.png"
+  },
+  {
+    "revision": "70a248c9743b93657092b86d57714bb8",
+    "url": "/portfolio/static/media/bgr_page.70a248c9.jpg"
+  },
+  {
+    "revision": "2caa4a364c2eece479cb991ac530563c",
+    "url": "/portfolio/static/media/campingShopBig.2caa4a36.png"
+  },
+  {
+    "revision": "36d8dc391a3acc3643c2516f9ba26e6f",
+    "url": "/portfolio/static/media/campingShopMini.36d8dc39.png"
+  },
+  {
+    "revision": "ab462b09358c3e81ee2adbc7cf205502",
+    "url": "/portfolio/static/media/cfBig.ab462b09.png"
+  },
+  {
+    "revision": "8e42a5b4c1eced85f488add3d33995fd",
+    "url": "/portfolio/static/media/cfMini.8e42a5b4.png"
+  },
+  {
+    "revision": "a838f025be6f1711d72e38f457744b13",
+    "url": "/portfolio/static/media/clouderChatBig.a838f025.png"
+  },
+  {
+    "revision": "7c548b7a6d15e26cd89da45c7becb05a",
+    "url": "/portfolio/static/media/clouderChatMini.7c548b7a.png"
+  },
+  {
+    "revision": "aeb1373726335c8f8a45f8bcbe07adc6",
+    "url": "/portfolio/static/media/cmBig.aeb13737.png"
+  },
+  {
+    "revision": "29f1046308503b89f6e9ac2183de1619",
+    "url": "/portfolio/static/media/cmMini.29f10463.png"
+  },
+  {
+    "revision": "813c048919a1fac59376acbf7cee8514",
+    "url": "/portfolio/static/media/contacs_mail.813c0489.png"
+  },
+  {
+    "revision": "fce12eb9b2a02dd2f984e5b6d1f0b6d5",
+    "url": "/portfolio/static/media/contacts_github.fce12eb9.png"
+  },
+  {
+    "revision": "58fdaadcc4f37ddb53cd254b66985806",
+    "url": "/portfolio/static/media/drawingShapesBig.58fdaadc.png"
+  },
+  {
+    "revision": "c6f0979c30b33c15be9038f60b5cf093",
+    "url": "/portfolio/static/media/gameListBig.c6f0979c.png"
+  },
+  {
+    "revision": "fe6efee837f0760b01dfdfaabe9079a4",
+    "url": "/portfolio/static/media/hangmanBig.fe6efee8.png"
+  },
+  {
+    "revision": "ef7796bd7c040af05b5c607f73a5778d",
+    "url": "/portfolio/static/media/icon_about_inactive.ef7796bd.png"
+  },
+  {
+    "revision": "159dad49048c4e5df4fabdfdabfe7477",
+    "url": "/portfolio/static/media/icon_java.159dad49.png"
+  },
+  {
+    "revision": "cb8258524a760ba4c2e621718171023d",
+    "url": "/portfolio/static/media/icon_projects_active.cb825852.png"
+  },
+  {
+    "revision": "ac8fee07b659658739bcc0186c749fef",
+    "url": "/portfolio/static/media/icon_projects_inactive.ac8fee07.png"
+  },
+  {
+    "revision": "38bc45b332dd6b7f2db76396e5cbe528",
+    "url": "/portfolio/static/media/icon_reactjs.38bc45b3.png"
+  },
+  {
+    "revision": "190f026176e67587842f9682a3aef535",
+    "url": "/portfolio/static/media/icon_spring.190f0261.png"
+  },
+  {
+    "revision": "28e52709e01215028461b7b941db1c00",
+    "url": "/portfolio/static/media/mazeSolverBig.28e52709.png"
+  },
+  {
+    "revision": "f59fde8a97b4e041efba0713bdea5337",
+    "url": "/portfolio/static/media/mazeSolverMini.f59fde8a.png"
+  },
+  {
+    "revision": "16c8e8f47a89ef91c1fbbd865e1fd9db",
+    "url": "/portfolio/static/media/millionaireBig.16c8e8f4.png"
+  },
+  {
+    "revision": "fc6698e44310b4790e389ff98979acbd",
+    "url": "/portfolio/static/media/millionaireMini.fc6698e4.png"
+  },
+  {
+    "revision": "64e837ff3af2d2f057564be8cb16186b",
+    "url": "/portfolio/static/media/onlearnMini.64e837ff.png"
+  },
+  {
+    "revision": "4ac95e6856f9475381cba32222f86bf6",
+    "url": "/portfolio/static/media/pathFinderBig.4ac95e68.png"
+  },
+  {
+    "revision": "af798901f1c07d94ec5e61a941267f46",
+    "url": "/portfolio/static/media/pathFinderMini.af798901.png"
+  },
+  {
+    "revision": "403fbdba09ec5a99ad0dc6963226c264",
+    "url": "/portfolio/static/media/randomCalculatorBig.403fbdba.png"
+  },
+  {
+    "revision": "e4cdad0ed03e24684ff885a78649a211",
+    "url": "/portfolio/static/media/randomCalculatorMini.e4cdad0e.png"
+  }
+]);
