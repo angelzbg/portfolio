f50db2174a2f1a75a01c4fd7842e19170b6b3523
angelzbg <badblo0dbg@gmail.com>
2020-02-29 15:48:15 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,17 +10,12 @@ export default class Footer extends React.Component {
         return(
             <div className="footer">
                 <center>
-                <h4>Contacts</h4>
-                <div className="contactsWrap">
-                    <a className="contact" href="mailto:angel.zlatanovv@gmail.com"><img src={contacs_mail} alt=""/></a>
-                    <a className="contact" href="https://github.com/angelzbg" target="_blank" rel="noopener noreferrer"><img src={contacts_github} alt=""/></a>
-                    <a className="contact" href="https://www.facebook.com/angel.zlatanovv" target="_blank" rel="noopener noreferrer"><img src={contacts_fb} alt=""/></a>
-                </div>
-                
-                <div style={{paddingTop: "15px", color: "#58aedb"}}>
-                    This site is built using ReactJS. Source code can be found <a style={{color: "#75e0d0", fontWeight: "bold", textDecoration: "none"}} href="https://github.com/angelzbg/portfolio" target="_blank" rel="noopener noreferrer">here</a>.
-                </div>
-
+                    <h4>Contacts</h4>
+                    <div className="contactsWrap">
+                        <a className="contact" href="mailto:angel.zlatanovv@gmail.com"><img src={contacs_mail} alt=""/></a>
+                        <a className="contact" href="https://github.com/angelzbg" target="_blank" rel="noopener noreferrer"><img src={contacts_github} alt=""/></a>
+                        <a className="contact" href="https://www.facebook.com/angel.zlatanovv" target="_blank" rel="noopener noreferrer"><img src={contacts_fb} alt=""/></a>
+                    </div>
                 </center>
             </div>
         )
