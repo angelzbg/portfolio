b234b9e3797c9ef08023a11df2c3455d2ed4c989
angelzbg <38349591+angelzbg@users.noreply.github.com>
2020-12-14 23:35:06 +0200

    Updates

---
+++ precache-manifest.6fed1811cb94b019d60251e0db6efeee.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223a783ac93346f6c04dfe517cbc4781",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "350e3bdd5a31f06d09d1",
+    "url": "/portfolio/static/css/2.e18a25e7.chunk.css"
+  },
+  {
+    "revision": "6ad6fb5157e61a5c7a8f",
+    "url": "/portfolio/static/css/main.ae6e03f5.chunk.css"
+  },
+  {
+    "revision": "350e3bdd5a31f06d09d1",
+    "url": "/portfolio/static/js/2.a5b4a345.chunk.js"
+  },
+  {
+    "revision": "6ad6fb5157e61a5c7a8f",
+    "url": "/portfolio/static/js/main.8df8c3ad.chunk.js"
+  },
+  {
+    "revision": "9821c9659a93692e486a",
+    "url": "/portfolio/static/js/runtime-main.14a56db7.js"
+  },
+  {
+    "revision": "b79d6d6b5d063b2e27e8e093138afc97",
+    "url": "/portfolio/static/media/angel.b79d6d6b.jpg"
+  },
+  {
+    "revision": "f72431a2a4b5d5e8e2c81905cf66023e",
+    "url": "/portfolio/static/media/github.f72431a2.png"
+  },
+  {
+    "revision": "ccacd7803ed4a25c9d5ec0162fed445c",
+    "url": "/portfolio/static/media/gmail.ccacd780.png"
+  },
+  {
+    "revision": "75e4d6d5a0f6b8f4e4030f9b00511ca7",
+    "url": "/portfolio/static/media/icon_css.75e4d6d5.png"
+  },
+  {
+    "revision": "2eb4f0891e3ac62e588d81bce705b16b",
+    "url": "/portfolio/static/media/icon_firebase.2eb4f089.png"
+  },
+  {
+    "revision": "acf172f26b6761ec94a0d4316bdbf6d4",
+    "url": "/portfolio/static/media/icon_github.acf172f2.png"
+  },
+  {
+    "revision": "31039a53ab57f00b4eff33fb7bcfa2e8",
+    "url": "/portfolio/static/media/icon_html.31039a53.png"
+  },
+  {
+    "revision": "6a4c201cef84c174d0d93923f26b168c",
+    "url": "/portfolio/static/media/icon_javascript.6a4c201c.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/portfolio/static/media/icon_nodejs.55e83371.png"
+  },
+  {
+    "revision": "8b1f11dacfcfb87a719f672e3a33320c",
+    "url": "/portfolio/static/media/linkedin.8b1f11da.png"
+  },
+  {
+    "revision": "7a7d05ce48e495f4dc851b5da572f717",
+    "url": "/portfolio/static/media/pu_logo.7a7d05ce.png"
+  },
+  {
+    "revision": "e4bbecaad5e4f9a8c25bf3d828c69a61",
+    "url": "/portfolio/static/media/softuni_logo.e4bbecaa.png"
+  },
+  {
+    "revision": "c269931b55820d5864d0712a6387b924",
+    "url": "/portfolio/static/media/stackoverflow.c269931b.png"
+  },
+  {
+    "revision": "9d05a0018fd90c1295751197b02f68e5",
+    "url": "/portfolio/static/media/stackoverflow1.9d05a001.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0327b9e071323e6cd32f4be9ddc61f0b.js"
+  "/portfolio/precache-manifest.6fed1811cb94b019d60251e0db6efeee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
