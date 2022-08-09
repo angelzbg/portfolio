fa405ebe21457c00e705601a98027bd131e036a1
Angel Zlatanov <38349591+angelzbg@users.noreply.github.com>
2022-08-09 17:03:01 +0300

    Update data.ts

--- src/components/menu/data.ts
+++ src/components/menu/data.ts
@@ -1,5 +1,5 @@
 const contacts: { href: string; ext: boolean }[] = [
-  { href: 'https://www.facebook.com/angel.zlatanovv', ext: true },
+  { href: 'https://www.facebook.com/angelzbg', ext: true },
   { href: 'https://www.linkedin.com/in/angel-zlatanov-9379ba1ba/', ext: true },
   { href: 'https://github.com/angelzbg', ext: true },
   { href: 'https://stackoverflow.com/users/14342112/angel-zlatanov?tab=profile', ext: true },
