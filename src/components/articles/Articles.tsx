cc24593fc3ee8afb9118167bd65aca24cd3046e9
Angel Zlatanov <38349591+angelzbg@users.noreply.github.com>
2023-08-01 21:40:36 +0300

    Update Articles.tsx

--- src/components/articles/Articles.tsx
+++ src/components/articles/Articles.tsx
@@ -6,8 +6,8 @@ const Articles = ({ isOpen }: { isOpen: boolean }) => (
   <div className="dropdown-menu-wrapper articles-dropdown-wrapper" style={{ display: isOpen ? 'block' : 'none' }}>
     <div className="dropdown-menu-wrap articles-dropdown-menu-wrap">
       <div className="articles scrollbar">
-        <RedditArticle url="https://www.reddit.com/r/reactjs/comments/jl1ajl/theming_in_react_with_variables_with_default_dark" />
         <RedditArticle url="https://www.reddit.com/r/reactjs/comments/15fkc3m/easy_mobx_setup_in_react/" />
+        <RedditArticle url="https://www.reddit.com/r/reactjs/comments/jl1ajl/theming_in_react_with_variables_with_default_dark" />
       </div>
     </div>
   </div>
